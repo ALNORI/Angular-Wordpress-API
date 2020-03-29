89521c6288e8686672352f3b65fbfd2de2b32201
Abdulbasit <abuamer04@hotmail.com>
2020-03-29 23:57:49 +0200

    added video features in posts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,12 @@
 import { Component } from '@angular/core';
-import { Observable } from 'rxjs';
-import { NgForm } from '@angular/forms';
-import { HttpClientModule, HttpClient } from '@angular/common/http'; 
-import { Headers } from '@angular/http';
+import { WpService } from './services/wpservice.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {  
-  token = null;
-
-  constructor() {
-  }
-
+export class AppComponent{
+  
+  title = 'eunice';
 }
